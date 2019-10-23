57ac7c13f44b9c347d3f7dc22af8f9407f6c6e59
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-23 17:01:09 -0400

    Update rain.js

--- rain.js
+++ rain.js
@@ -4,7 +4,7 @@ let totalDrops = 500;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (i = 0; i < totalDrops; i ++) {
-    dropsA[i].push(new Drop());
+    dropsA.push(new Drop());
   }
 }
 
