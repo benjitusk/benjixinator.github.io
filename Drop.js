557fe9ab365ef48b16f7ff6569c3bee7771b1211
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-23 17:08:43 -0400

    Update Drop.js

--- Drop.js
+++ Drop.js
@@ -1,22 +1,36 @@
 class Drop {
-
-  constructor() {
-    this.xoff;
-    this.x = random(width);
-    this.y = random(-height -50, 0);
-    this.len = random(4, 10);
-    this.speed = random(5, 20);
-    //this.c = color(random(255), random(255), random(255));
-
-  }
-
-  fall() {
-    this.y += this.speed;
-  }
-
-  show() {
-    stroke(255);
-    line(this.x, this.y, this.x, this.y - this.len);
-  }
-
+    constructor () {
+        this.pos = createVector(random(width), random(-height - 50, -50));
+        this.speed = random(5, 20);
+        this.len = random(10, 20);
+        this.c = color(random(255), random(255), random(255));
+    }
+    
+    fall() {
+        this.pos.y += this.speed;
+        
+        if (this.pos.y > height) {
+            // Randomize and reconstruct the drop
+            this.pos = createVector(random(width), random(-height - 50, -50));
+            this.speed = random(5, 20);
+            this.len = random(10, 20);
+            this.c = color(random(255), random(255), random(255));
+        }
+        
+    }
+    
+    show() {
+        stroke(this.c);
+        strokeWeight(5);
+        line(this.pos.x, this.pos.y, this.pos.x, this.pos.y + this.len);
+    }
+    
+    repel() {
+        
+        if (this.pos.x <= mouseX) {     // if drop is to the left of the mouse
+            this.pos.x -= this.speed;            // push it to the left
+        } else {                        // and if it's to the right, 
+            this.pos.x += this.speed;            // push it to the right
+        }
+    }
 }
