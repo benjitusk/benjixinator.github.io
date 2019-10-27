b852829e9f9ce6319f706e4e483b0a681c37e758
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-27 19:47:41 -0400

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -11,9 +11,9 @@ class Ball {
         if (running) {
             this.xoff += 0.1;
             if (this.x <= 0) { // If the ball is in danger of going off the screen to the <<LEFT
-                this.x = width + 10;
+                this.x += 10;
             } else if (this.x >= width) { // If the ball is in danger of going off the screen to the RIGHT>>
-                this.x = 0;
+                this.x -= 10;
             } else { // If the ball is in the <<CENTER>>
                 this.x += map(noise(this.xoff), 0, 1, -5, 7.5);
             }
