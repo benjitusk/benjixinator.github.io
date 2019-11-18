c12e746baa20eb672a64a9b6122ab5ad0c9ba42f
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-11-17 21:32:30 -0500

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,26 +1,18 @@
 class Ball {
     constructor() {
-        this.xoff = random(0, 999999);
         this.r = 20;
-        this.x = width / 2;
-        this.y = (height * 11 / 12) - this.r;
+        this.x = mouseY;
+        this.y = mouseX;
         this.health = 100;
     }
     
+    update() {
+        this.x = mouseY;
+        this.y = mouseX;
+    }
+    
     show() {
-        if (running) {
-            this.xoff += 0.1;
-            if (this.x <= 0) { // If the ball is in danger of going off the screen to the <<LEFT
-                this.x = 5;
-            } else if (this.x >= width) { // If the ball is in danger of going off the screen to the RIGHT>>
-                this.x = width - 5;
-                console.log(this.x);
-            } else { // If the ball is in the <<CENTER>>
-                this.x += map(noise(this.xoff), 0, 1, -5, 6.8);
-            }
-        }
-            noStroke();
-            fill(138, 43, 226);
-            ellipse(this.x, this.y - (this.r / 5), this.r * 2);
+     
+        ellipse(this.x, this.y, this.r * 2);
     }
 }
