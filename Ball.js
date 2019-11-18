678792584676e14e48ae52814b5de980fd967f27
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-11-17 22:09:07 -0500

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,18 +1,19 @@
 class Ball {
     constructor() {
         this.r = 20;
-        this.x = mouseY;
-        this.y = mouseX;
+        this.x = mouseX;
+        this.y = mouseY;
         this.health = 100;
     }
     
     update() {
-        this.x = mouseY;
-        this.y = mouseX;
+        this.x = mouseX;
+        this.y = mouseY;
     }
     
     show() {
-     
+	    noStroke();
+     	fill(0, 0, 255);
         ellipse(this.x, this.y, this.r * 2);
     }
 }
